b3f005f8ef4d9bef1283cf9df80f17f9c05819e0
Phuttipan Chan-Ngam <phuttipan.ch@gmail.com>
2023-04-28 20:17:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('The efficacy of molnupiravir as a first-line drug for short-term management of effusive form feline infectious peritonitis: a case series')
 
 st.write('Hello world!')
