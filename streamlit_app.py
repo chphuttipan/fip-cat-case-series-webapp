b2b33a7ae0d150a3108977cc85d236c6f2db86a1
Phuttipan Chan-Ngam <phuttipan.ch@gmail.com>
2023-04-29 15:42:59 +0000

    mu_molnupiravir_fip_project_v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('The efficacy of molnupiravir as a first-line drug for short-term management of effusive form feline infectious peritonitis: a case series')
 
-st.write('Hello world!')
+df = pd.read_csv("https://raw.githubusercontent.com/chphuttipan/data/main/mu_fip_project2.csv")
+
+st.write(
+    "Hello!, This is the web app to express a data visualization about this project"
+)
+
+
